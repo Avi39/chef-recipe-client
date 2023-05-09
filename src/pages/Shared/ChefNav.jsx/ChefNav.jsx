ea04d78e40f6667c3532c3409a31b01949c66036
Avi <avishekuser@gmai.com>
2023-05-10 03:14:13 +0600

    banner done

--- src/pages/Shared/ChefNav.jsx/ChefNav.jsx
+++ src/pages/Shared/ChefNav.jsx/ChefNav.jsx
@@ -1,7 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ChefNav = () => {
+    const [chefs,setChefs] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/chefs')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error=>console.error(error))
+    },[])
+    
     return (
         <div>
             <h2>Chef nav</h2>
